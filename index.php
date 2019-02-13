fff67646d9ea11b032e18451b356a6e3f70494bf
wadjakorn <wazjakorn@gmail.com>
2019-02-13 15:47:00 +0700

    test jenkins

--- index.php
+++ index.php
@@ -7,14 +7,33 @@
     
     <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
     <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/styles.css" />
     
-    <title>D-baze.me</title>
+    <title>D-baze.me test</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <button class="btn btn-primary">Submit</button>
+    <div class="container">
+      <div class="f-center v welcome-container">
+        <div class="f-center v inner-container">
+          <!-- <h1>Welcome to</h1> -->
+          <Avatar>
+            <?php 
+              $images = (object) array(
+                'me' => 'resources/images/me',
+              );
+            ?>
+            <img src="<?php echo $images->me; ?>/prof1.jpg" alt="">
+          </Avatar>
+          <!-- <h1>D-baze.me</h1> -->
+        </div>
+      </div>
+    </div>
+    
     <script src="js/jquery.min.js" ></script>
     <script src="js/popper.min.js" ></script>
     <script src="js/bootstrap.min.js" ></script>
   </body>
+  <script>
+
+  </script>
 </html>
